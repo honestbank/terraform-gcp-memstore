6d8aefbd36fe12f3815bb5e0f771b529deeaceb7
James <admin@floretos.com>
2022-04-11 11:45:52 +0700

    feat: create redis memstore on gcp (#1)

---
+++ modules/memstore_redis/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+resource "google_redis_instance" "cache" {
+  name           = replace(var.name, " ", "-")
+  tier           = var.tier
+  memory_size_gb = var.memory_size
+
+  auth_enabled = true
+
+  location_id             = "${var.region}-${var.zone}"
+  alternative_location_id = "${var.region}-${var.zone}"
+
+  replica_count      = var.tier == "STANDARD_HA" ? var.replicas : null
+  read_replicas_mode = var.read_replicas_enabled ? "READ_REPLICAS_ENABLED" : "READ_REPLICAS_DISABLED"
+
+  authorized_network = var.network_id
+
+  transit_encryption_mode = var.encryption ? "SERVER_AUTHENTICATION" : "DISABLED"
+
+
+  redis_version     = var.redis_version
+  display_name      = var.name
+  reserved_ip_range = var.reserved_ip_range
+
+}
---
+++ test/main_test.go
@@ -0,0 +1,47 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTerraformCreateGCPSQL(t *testing.T) {
+	t.Parallel()
+
+	t.Run("create mysql with public ip", func(t *testing.T) {
+		t.Parallel()
+
+		testDirectory := test_structure.CopyTerraformFolderToTemp(t, "..", "examples/create_redis_public_ip")
+
+		// retryable errors in terraform testing.
+		terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+			TerraformDir: testDirectory,
+			Vars: map[string]interface{}{
+				"google_region": "asia-southeast2",
+			},
+		})
+
+		defer terraform.Destroy(t, terraformOptions)
+
+		terraform.InitAndApply(t, terraformOptions)
+
+		var output string
+
+		output = terraform.Output(t, terraformOptions, "id")
+		assert.NotEmpty(t, output)
+
+		output = terraform.Output(t, terraformOptions, "name")
+		assert.NotEmpty(t, output)
+
+		output = terraform.Output(t, terraformOptions, "host")
+		assert.NotEmpty(t, output)
+
+		output = terraform.Output(t, terraformOptions, "port")
+		assert.NotEmpty(t, output)
+
+	})
+
+}
