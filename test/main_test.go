3db55c65f42d95e9ca995037f1b820a49620f3c4
Christian Witts <christian@honestbank.com>
2024-07-24 17:21:01 +0700

    fix: Update precondition for BASIC tier
    The `BASIC` tier precondition failed due to an incorrect truth
    table. The condition has been updated and a test case added to
    validate a `BASIC` tier use-case.
    
    Refs: #DEVOP-4801
    
    Signed-off-by: Christian Witts <christian@honestbank.com>

--- test/main_test.go
+++ test/main_test.go
@@ -45,3 +45,41 @@ func TestTerraformCreateGCPMemstore(t *testing.T) {
 	})
 
 }
+
+func TestTerraformCreateGCPMemstoreBasic(t *testing.T) {
+	t.Parallel()
+
+	t.Run("create BASIC tier Redis with public ip", func(t *testing.T) {
+		t.Parallel()
+
+		testDirectory := test_structure.CopyTerraformFolderToTemp(t, "..", "examples/create_redis_basic_instance")
+
+		// retryable errors in terraform testing.
+		terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+			TerraformDir: testDirectory,
+			Vars: map[string]interface{}{
+				"google_region": "asia-southeast2",
+			},
+		})
+
+		defer terraform.Destroy(t, terraformOptions)
+
+		terraform.InitAndApply(t, terraformOptions)
+
+		var output string
+
+		output = terraform.Output(t, terraformOptions, "id")
+		assert.NotEmpty(t, output)
+
+		output = terraform.Output(t, terraformOptions, "name")
+		assert.NotEmpty(t, output)
+
+		output = terraform.Output(t, terraformOptions, "host")
+		assert.NotEmpty(t, output)
+
+		output = terraform.Output(t, terraformOptions, "port")
+		assert.NotEmpty(t, output)
+
+	})
+
+}
